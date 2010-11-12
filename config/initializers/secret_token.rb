ad297ef835abe2188bec6aa9f66ece3e8fbdaa2d
Utkarsh Kukreti <railsdevel@gmail.com>
2010-11-13 00:28:13 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '937c6907332e58167e350e510d4bf6ab0f39963633d9b1f24e82153d32af4118f0d52b0e5c0c1992c01a551d7f45bf5c344bdf40c04abf11cf7a4d46f3250f7e'
