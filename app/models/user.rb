637085d868ac7b185a4ad7c3705d184ae4e00373
Utkarsh Kukreti <railsdevel@gmail.com>
2010-11-14 03:07:15 +0530

    disable friendly_id until a way to choose user id is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :lists
 
-  has_friendly_id :name, :use_slug => true
+  # has_friendly_id :name, :use_slug => true
 
   def self.create_from_hash!(hash)
     create(:name => hash['user_info']['name'])
